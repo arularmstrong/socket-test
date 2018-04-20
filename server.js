18d122216010bf791b175d3dbe771fca4eff10db
armstrong <s.arularmstrong@gmail.com>
2018-04-20 13:54:42 +0300

    try

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var http = require('http'),
 app.set('port', port);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(express.favicon());
+
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
